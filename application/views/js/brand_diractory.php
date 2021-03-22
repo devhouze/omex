b8014d02d256dd89d3ded69282d176208010f4e2
anurag_wibytes <anurag@wibytes.com>
2021-03-22 23:01:16 +0530

    Fillter Done we need to testing on live server

--- application/views/js/brand_diractory.php
+++ application/views/js/brand_diractory.php
@@ -85,182 +85,188 @@
     });
 </script>
 <script>
-$(document).ready(function(){
-    $('.select').select2();
-});
+    $(document).ready(function(){
+        $('.select').select2();
+    });
 </script>
 
 <script>
-$(document).ready(function(){
-    const base_url = $('body').data('url');
-    $('#street').change(function(){
-        get_brands();
-    });
+    $(document).ready(function(){
+        const base_url = $('body').data('url');
+        $('#street').change(function(){
+            $('.letter').removeClass('active');
+            $('.category').removeClass('active');
+            get_brands();
+        });
 
-    $('#sort').change(function(){
-        get_brands();
-    });
+        $('#sort').change(function(){
+            $('.letter').removeClass('active');
+            $('.category').removeClass('active');
+            get_brands();
+        });
 
-    
-    $('.primary-btn').click(function(){
-        var street = $('#street').val();
-        var sort = $('#sort').val();
-        var filter = $('#filter').val();
-        var limit = $('#limit').val();
-        console.log(limit);
-        $('#brand').empty();
-        $.ajax({
-            type:'post',
-            url:'<?php echo base_url('search-brand')?>',
-            data:{street:street, sort:sort,filter:filter,limit:limit},
-            dataType:'json',
-            success:function(data){
-                if(data.brand != ''){
-                    $.each(data.brand,function(i,v){
-                    var value = '<div class="col-md-3 col-6">';
-                    value += '<div class="product-box">';
-                   value += "<a href='"+base_url+"brand/"+v.brand_slug+"'></a>";
-                    value += "<figure><img src='"+base_url+"/assets/images/public/brand/"+v.brand_logo+"' alt='"+v.logo_message+"'></figure>";
-                    value += '<div class="name">'+v.brand_name+'</div>';
-                    value += '<div class="addrs"><img src="'+base_url+'assets/images/public/brand/map.svg" alt="" class="">'+v.brand_location+','+v.brand_street+'</div>';
-                    value += '</div>';
-                    value += '</div>';
-                    $('#brand').append(value);
-                    });
-                    $('#limit').val(data.limit);
-                    if(data.limit == ''){
-                        $('#load_btn').css('display','none');
+        
+        $('.primary-btn').click(function(){
+            var street = $('#street').val();
+            var sort = $('#sort').val();
+            var filter = $('#filter').val();
+            var limit = $('#limit').val();
+            console.log(limit);
+            $('#brand').empty();
+            $.ajax({
+                type:'post',
+                url:'<?php echo base_url('search-brand')?>',
+                data:{street:street, sort:sort,filter:filter,limit:limit},
+                dataType:'json',
+                success:function(data){
+                    if(data.brand != ''){
+                        $.each(data.brand,function(i,v){
+                            var value = '<div class="col-md-3 col-6">';
+                            value += '<div class="product-box">';
+                            value += "<a href='"+base_url+"brand/"+v.brand_slug+"'></a>";
+                            value += "<figure><img src='"+base_url+"/assets/images/public/brand/"+v.brand_logo+"' alt='"+v.logo_message+"'></figure>";
+                            value += '<div class="name">'+v.brand_name+'</div>';
+                            value += '<div class="addrs"><img src="'+base_url+'assets/images/public/brand/map.svg" alt="" class="">'+v.brand_location+','+v.brand_street+'</div>';
+                            value += '</div>';
+                            value += '</div>';
+                            $('#brand').append(value);
+                        });
+                        $('#limit').val(data.limit);
+                        if(data.limit == ''){
+                            $('#load_btn').css('display','none');
+                        } else {
+                            $('#load_btn').css('display','block');
+                        }
                     } else {
-                        $('#load_btn').css('display','block');
+                        $('#brand').html('No match found!.');
                     }
-                } else {
-                    $('#brand').html('No match found!.');
                 }
-            }
-        })
-    });
+            })
+        });
 
-    $('.letter').click(function(){
-        var street = $('#street').val();
-        var sort = $('#sort').val();
-        var filter = $('#filter').val();
-        var letter = $(this).data('letter');
-        var limit = $('#limit').val();
-        $('.letter').removeClass('active');
-        $(this).addClass("active");   
-        $('#brand').empty();
-        $.ajax({
-            type:'post',
-            url:'<?php echo base_url('search-brand')?>',
-            data:{street:street, sort:sort,filter:filter,limit:limit,letter:letter},
-            dataType:'json',
-            success:function(data){
-                if(data.brand != ''){
-                    $.each(data.brand,function(i,v){
-                    var value = '<div class="col-md-3 col-6">';
-                    value += '<div class="product-box">';
-                    value += "<a href='"+base_url+"brand/"+v.brand_slug+"'></a>";
-                    value += "<figure><img src='"+base_url+"/assets/images/public/brand/"+v.brand_logo+"' alt='"+v.logo_message+"'></figure>";
-                    value += '<div class="name">'+v.brand_name+'</div>';
-                    value += '<div class="addrs"><img src="'+base_url+'assets/images/public/brand/map.svg" alt="" class="">'+v.brand_location+','+v.brand_street+'</div>';
-                    value += '</div>';
-                    value += '</div>';
-                    $('#brand').append(value);
-                    });
-                    $('#limit').val(data.limit);
-                    if(data.limit == ''){
-                        $('#load_btn').css('display','none');
+        $('.letter').click(function(){
+            var street = $('#street').val();
+            var sort = $('#sort').val();
+            var filter = $('#filter').val();
+            var letter = $(this).data('letter');
+            var limit = $('#limit').val();
+            $('.letter').removeClass('active');
+            $(this).addClass("active");   
+            $('#brand').empty();
+            $.ajax({
+                type:'post',
+                url:'<?php echo base_url('search-brand')?>',
+                data:{street:street, sort:sort,filter:filter,limit:limit,letter:letter},
+                dataType:'json',
+                success:function(data){
+                    if(data.brand != ''){
+                        $.each(data.brand,function(i,v){
+                            var value = '<div class="col-md-3 col-6">';
+                            value += '<div class="product-box">';
+                            value += "<a href='"+base_url+"brand/"+v.brand_slug+"'></a>";
+                            value += "<figure><img src='"+base_url+"/assets/images/public/brand/"+v.brand_logo+"' alt='"+v.logo_message+"'></figure>";
+                            value += '<div class="name">'+v.brand_name+'</div>';
+                            value += '<div class="addrs"><img src="'+base_url+'assets/images/public/brand/map.svg" alt="" class="">'+v.brand_location+','+v.brand_street+'</div>';
+                            value += '</div>';
+                            value += '</div>';
+                            $('#brand').append(value);
+                        });
+                        $('#limit').val(data.limit);
+                        if(data.limit == ''){
+                            $('#load_btn').css('display','none');
+                        } else {
+                            $('#load_btn').css('display','block');
+                        }
                     } else {
-                        $('#load_btn').css('display','block');
-                    }
-                } else {
-                    $('#brand').html('No match found!.');
-                    $('#load_btn').css('display','none');
-                }
-            }
-        })
-    });
-
-    $('.category').click(function(){
-        var street = $('#street').val();
-        var sort = $('#sort').val();
-        var filter = $('#filter').val();
-        var category = $(this).data('category');
-        var limit = $('#limit').val();
-        $('#brand').empty();
-        $.ajax({
-            type:'post',
-            url:'<?php echo base_url('search-brand')?>',
-            data:{street:street, sort:sort,filter:filter,limit:limit,category:category},
-            dataType:'json',
-            success:function(data){
-                if(data.brand != ''){
-                    $.each(data.brand,function(i,v){
-                    var value = '<div class="col-md-3 col-6">';
-                    value += '<div class="product-box">';
-                    value += "<a href='"+base_url+"brand/"+v.brand_slug+"'></a>";
-                    value += "<figure><img src='"+base_url+"/assets/images/public/brand/"+v.brand_logo+"' alt='"+v.logo_message+"'></figure>";
-                    value += '<div class="name">'+v.brand_name+'</div>';
-                    value += '<div class="addrs"><img src="'+base_url+'assets/images/public/brand/map.svg" alt="" class="">'+v.brand_location+','+v.brand_street+'</div>';
-                    value += '</div>';
-                    value += '</div>';
-                    $('#brand').append(value);
-                    });
-                    $('#limit').val(data.limit);
-                    if(data.limit == ''){
+                        $('#brand').html('No match found!.');
                         $('#load_btn').css('display','none');
-                    } else {
-                        $('#load_btn').css('display','block');
                     }
-                } else {
-                    $('#brand').html('No match found!.');
-                    $('#load_btn').css('display','none');
                 }
-            }
-        })
-    });
+            })
+        });
 
-    $('#filter').change(function(){
-        get_brands();
-    });
-
-    function get_brands(){
-        var street = $('#street').val();
-        var sort = $('#sort').val();
-        var filter = $('#filter').val();
-        var limit = $('#limit').val();
-        $('#brand').empty();
-        $.ajax({
-            type:'post',
-            url:'<?php echo base_url('search-brand')?>',
-            data:{street:street, sort:sort,filter:filter,limit:limit},
-            dataType:'json',
-            success:function(data){
-                if(data.brand != ''){
-                    $.each(data.brand,function(i,v){
-                    var value = '<div class="col-md-3 col-6">';
-                    value += '<div class="product-box">';
-                    value += "<a href='"+base_url+"brand/"+v.brand_slug+"'></a>";
-                    value += "<figure><img src='"+base_url+"/assets/images/public/brand/"+v.brand_logo+"' alt='"+v.logo_message+"'></figure>";
-                    value += '<div class="name">'+v.brand_name+'</div>';
-                    value += '<div class="addrs"><img src="'+base_url+'assets/images/public/brand/map.svg" alt="" class="">'+v.brand_location+','+v.brand_street+'</div>';
-                    value += '</div>';
-                    value += '</div>';
-                    $('#brand').append(value);
-                    });
-                    $('#limit').val(data.limit);
-                    if(data.limit == ''){
+        $('.category').click(function(){
+            var street = $('#street').val();
+            var sort = $('#sort').val();
+            var filter = $('#filter').val();
+            var category = $(this).data('category');
+            $('.category').removeClass('active');
+            $(this).addClass("active");
+            var limit = $('#limit').val();
+            $('#brand').empty();
+            $.ajax({
+                type:'post',
+                url:'<?php echo base_url('search-brand')?>',
+                data:{street:street, sort:sort,filter:filter,limit:limit,category:category},
+                dataType:'json',
+                success:function(data){
+                    if(data.brand != ''){
+                        $.each(data.brand,function(i,v){
+                            var value = '<div class="col-md-3 col-6">';
+                            value += '<div class="product-box">';
+                            value += "<a href='"+base_url+"brand/"+v.brand_slug+"'></a>";
+                            value += "<figure><img src='"+base_url+"/assets/images/public/brand/"+v.brand_logo+"' alt='"+v.logo_message+"'></figure>";
+                            value += '<div class="name">'+v.brand_name+'</div>';
+                            value += '<div class="addrs"><img src="'+base_url+'assets/images/public/brand/map.svg" alt="" class="">'+v.brand_location+','+v.brand_street+'</div>';
+                            value += '</div>';
+                            value += '</div>';
+                            $('#brand').append(value);
+                        });
+                        $('#limit').val(data.limit);
+                        if(data.limit == ''){
+                            $('#load_btn').css('display','none');
+                        } else {
+                            $('#load_btn').css('display','block');
+                        }
+                    } else {
+                        $('#brand').html('No match found!.');
                         $('#load_btn').css('display','none');
-                    } else {
-                        $('#load_btn').css('display','block');
                     }
-                } else {
-                    $('#brand').html('No match found!.');
-                    $('#load_btn').css('display','none');
                 }
-                
-            }
-        })
-    }
-});
+            })
+        });
+
+        $('#filter').change(function(){
+            get_brands();
+        });
+
+        function get_brands(){
+            var street = $('#street').val();
+            var sort = $('#sort').val();
+            var filter = $('#filter').val();
+            var limit = $('#limit').val();
+            $('#brand').empty();
+            $.ajax({
+                type:'post',
+                url:'<?php echo base_url('search-brand')?>',
+                data:{street:street, sort:sort,filter:filter,limit:limit},
+                dataType:'json',
+                success:function(data){
+                    if(data.brand != ''){
+                        $.each(data.brand,function(i,v){
+                            var value = '<div class="col-md-3 col-6">';
+                            value += '<div class="product-box">';
+                            value += "<a href='"+base_url+"brand/"+v.brand_slug+"'></a>";
+                            value += "<figure><img src='"+base_url+"/assets/images/public/brand/"+v.brand_logo+"' alt='"+v.logo_message+"'></figure>";
+                            value += '<div class="name">'+v.brand_name+'</div>';
+                            value += '<div class="addrs"><img src="'+base_url+'assets/images/public/brand/map.svg" alt="" class="">'+v.brand_location+','+v.brand_street+'</div>';
+                            value += '</div>';
+                            value += '</div>';
+                            $('#brand').append(value);
+                        });
+                        $('#limit').val(data.limit);
+                        if(data.limit == ''){
+                            $('#load_btn').css('display','none');
+                        } else {
+                            $('#load_btn').css('display','block');
+                        }
+                    } else {
+                        $('#brand').html('No match found!.');
+                        $('#load_btn').css('display','none');
+                    }
+                    
+                }
+            })
+        }
+    });
 </script>
