7c14a91d1d5f64a189957cd84f6b884486abc7bf
infodevsujit <infodevsujit@gmail.com>
2021-05-24 03:59:26 +0530

    working-on-task

--- application/views/whatenew.php
+++ application/views/whatenew.php
@@ -114,7 +114,7 @@
 
         <!-- Modal to show register form -->
         <div class="modal fade" id="registernow" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
+            <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
 
 
